e5fa6fb8eff586992042d0f4f2821a530cbdaf9a
mjwillis <michael.willis@digital.justice.gov.uk>
2024-03-14 10:14:55 +0000

    Update description in ErrorResponse schema
    This commit modifies the '@Schema' annotation in ErrorResponse, replacing 'resource' by 'ErrorCode' in the description. This change improves accuracy in code documentation, ensuring it correctly points to the 'ErrorCode' enumeration for error type identification.

--- src/main/kotlin/uk/gov/justice/digital/hmpps/incidentreporting/resource/ErrorResponse.kt
+++ src/main/kotlin/uk/gov/justice/digital/hmpps/incidentreporting/resource/ErrorResponse.kt
@@ -11,7 +11,7 @@ data class ErrorResponse(
   val userMessage: String,
   @Schema(description = "More detailed error message", example = "[Details, sometimes a stack trace]", required = true)
   val developerMessage: String,
-  @Schema(description = "When present, uniquely identifies the type of error making it easier for clients to discriminate without relying on error description or HTTP status code; see ` uk.gov.justice.digital.hmpps.incidentreporting.resource` enumeration in hmpps-incident-reporting-api", example = "101", required = false)
+  @Schema(description = "When present, uniquely identifies the type of error making it easier for clients to discriminate without relying on error description or HTTP status code; see ` uk.gov.justice.digital.hmpps.incidentreporting.resource.ErrorCode` enumeration in hmpps-incident-reporting-api", example = "101", required = false)
   val errorCode: Int? = null,
   @Schema(description = "More information about the error", example = "[Rarely used, error-specific]", required = false)
   val moreInfo: String? = null,
