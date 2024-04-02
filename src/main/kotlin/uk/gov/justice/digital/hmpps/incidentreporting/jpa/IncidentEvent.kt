5e2747af76381fec757c89c53260e5faf7b5dcb7
Michael Willis <michael.willis@digital.justice.gov.uk>
2024-04-02 09:11:21 +0100

    IR-183 Add the concept of an event to the incident report. (#47)
    * IR-183 Add the concept of an event to the incident report.
    
    An incident can be linked to an event and an event can have multiple incidents
    
    * Update tests to allow linking to existing event

---
+++ src/main/kotlin/uk/gov/justice/digital/hmpps/incidentreporting/jpa/IncidentEvent.kt
@@ -0,0 +1,64 @@
+package uk.gov.justice.digital.hmpps.incidentreporting.jpa
+
+import jakarta.persistence.CascadeType
+import jakarta.persistence.Column
+import jakarta.persistence.Entity
+import jakarta.persistence.FetchType
+import jakarta.persistence.GeneratedValue
+import jakarta.persistence.GenerationType
+import jakarta.persistence.Id
+import jakarta.persistence.OneToMany
+import org.slf4j.Logger
+import org.slf4j.LoggerFactory
+import uk.gov.justice.digital.hmpps.incidentreporting.dto.EventDetail
+import java.io.Serializable
+import java.time.LocalDateTime
+
+@Entity
+class IncidentEvent(
+
+  @Id
+  @GeneratedValue(strategy = GenerationType.IDENTITY)
+  val id: Long? = null,
+
+  @Column(nullable = false, unique = true, length = 25)
+  val eventId: String,
+  val eventDateAndTime: LocalDateTime,
+  val prisonId: String,
+  var summary: String,
+  var eventDetails: String,
+
+  @OneToMany(mappedBy = "event", fetch = FetchType.LAZY, cascade = [CascadeType.ALL])
+  val incidents: MutableList<IncidentReport> = mutableListOf(),
+
+  val createdDate: LocalDateTime,
+  var lastModifiedDate: LocalDateTime,
+  var lastModifiedBy: String,
+
+) : Serializable {
+
+  companion object {
+    val log: Logger = LoggerFactory.getLogger(this::class.java)
+  }
+
+  override fun equals(other: Any?): Boolean {
+    if (this === other) return true
+    if (javaClass != other?.javaClass) return false
+
+    other as IncidentEvent
+
+    return eventId == other.eventId
+  }
+
+  override fun hashCode(): Int {
+    return eventId.hashCode()
+  }
+
+  fun toDto() = EventDetail(
+    eventId = eventId,
+    prisonId = prisonId,
+    eventDateAndTime = eventDateAndTime,
+    summary = summary,
+    eventDetails = eventDetails,
+  )
+}
