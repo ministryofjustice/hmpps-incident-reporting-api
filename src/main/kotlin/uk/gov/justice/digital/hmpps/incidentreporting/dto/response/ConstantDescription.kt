dff656938d2c3aed612f535a9fcab3165d573f47
Igor <igor.ushkarev@digital.justice.gov.uk>
2024-05-08 11:31:19 +0100

    Improve documentation

--- src/main/kotlin/uk/gov/justice/digital/hmpps/incidentreporting/dto/response/ConstantDescription.kt
+++ src/main/kotlin/uk/gov/justice/digital/hmpps/incidentreporting/dto/response/ConstantDescription.kt
@@ -1,14 +1,11 @@
 package uk.gov.justice.digital.hmpps.incidentreporting.dto.response
 
-import com.fasterxml.jackson.annotation.JsonProperty
 import io.swagger.v3.oas.annotations.media.Schema
 
 @Schema(description = "Code and description of a constant or enumeration member")
 data class ConstantDescription(
-  @Schema(description = "Machine-readable identifier of this value", required = true, example = "VICTIM")
-  @JsonProperty(required = true, access = JsonProperty.Access.READ_ONLY)
+  @Schema(description = "Machine-readable identifier of this value", required = true, readOnly = true, example = "VICTIM")
   val code: String,
-  @Schema(description = "Human-readable description of this value", required = true, example = "Victim")
-  @JsonProperty(required = true, access = JsonProperty.Access.READ_ONLY)
+  @Schema(description = "Human-readable description of this value", required = true, readOnly = true, example = "Victim")
   val description: String,
 )
