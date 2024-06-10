74d7274c9900835370b2fb72117e52d0d2b4ec3b
Igor <igor.ushkarev@digital.justice.gov.uk>
2024-06-10 11:33:01 +0100

    Turn time zone id into a separate, shared, bean

--- src/main/kotlin/uk/gov/justice/digital/hmpps/incidentreporting/config/TimeZoneConfiguration.kt
+++ src/main/kotlin/uk/gov/justice/digital/hmpps/incidentreporting/config/TimeZoneConfiguration.kt
@@ -1,14 +1,14 @@
 package uk.gov.justice.digital.hmpps.incidentreporting.config
 
+import org.springframework.beans.factory.annotation.Value
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
-import java.time.Clock
 import java.time.ZoneId
 
 @Configuration
-class ClockConfiguration(
-  private val zoneId: ZoneId,
+class TimeZoneConfiguration(
+  @Value("\${spring.jackson.time-zone}") private val timeZone: String,
 ) {
   @Bean
-  fun clock(): Clock = Clock.system(zoneId)
+  fun timeZone(): ZoneId = ZoneId.of(timeZone)
 }
