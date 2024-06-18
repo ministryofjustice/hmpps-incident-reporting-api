161f0b2aa6d534a51cb2c0b9db304041ea662668
Igor <igor.ushkarev@digital.justice.gov.uk>
2024-06-18 10:52:39 +0100

    Add a representation of a value that might have been changed …allowing a block to be called only if it had in fact been changed

---
+++ src/main/kotlin/uk/gov/justice/digital/hmpps/incidentreporting/dto/utils/MaybeChanged.kt
@@ -0,0 +1,24 @@
+package uk.gov.justice.digital.hmpps.incidentreporting.dto.utils
+
+/**
+ * Represents a value that might have been changed,
+ * allowing a block to be called only if it had been changed.
+ */
+sealed interface MaybeChanged<T> {
+  val value: T
+
+  fun alsoIfChanged(block: (T) -> Unit): MaybeChanged<T>
+
+  data class Unchanged<T>(override val value: T) : MaybeChanged<T> {
+    override fun alsoIfChanged(block: (T) -> Unit): Unchanged<T> {
+      return this
+    }
+  }
+
+  data class Changed<T>(override val value: T) : MaybeChanged<T> {
+    override fun alsoIfChanged(block: (T) -> Unit): Changed<T> {
+      block(value)
+      return this
+    }
+  }
+}
