81f66e6a25f55ecd6bd1b4d6b6e7c65796f5e17b
petergphillips <5099053+petergphillips@users.noreply.github.com>
2024-06-28 11:42:41 +0100

    hmpps-template-kotlin#215: Remove version from health and logging

--- src/test/kotlin/uk/gov/justice/digital/hmpps/incidentreporting/integration/health/HealthCheckTest.kt
+++ src/test/kotlin/uk/gov/justice/digital/hmpps/incidentreporting/integration/health/HealthCheckTest.kt
@@ -1,16 +1,11 @@
 package uk.gov.justice.digital.hmpps.incidentreporting.integration.health
 
-import org.assertj.core.api.Assertions.assertThat
 import org.junit.jupiter.api.Test
 import uk.gov.justice.digital.hmpps.incidentreporting.integration.SqsIntegrationTestBase
-import java.time.LocalDateTime
-import java.time.format.DateTimeFormatter
-import java.util.function.Consumer
 
 class HealthCheckTest : SqsIntegrationTestBase() {
-
   @Test
-  fun `Health page reports ok`() {
+  fun `health page reports ok`() {
     webTestClient.get()
       .uri("/health")
       .exchange()
@@ -21,19 +16,7 @@ class HealthCheckTest : SqsIntegrationTestBase() {
   }
 
   @Test
-  fun `Health info reports version`() {
-    webTestClient.get().uri("/health")
-      .exchange()
-      .expectStatus().isOk
-      .expectBody().jsonPath("components.healthInfo.details.version").value(
-        Consumer<String> {
-          assertThat(it).startsWith(LocalDateTime.now().format(DateTimeFormatter.ISO_DATE))
-        },
-      )
-  }
-
-  @Test
-  fun `Health ping page is accessible`() {
+  fun `health ping page is accessible`() {
     webTestClient.get()
       .uri("/health/ping")
       .exchange()
