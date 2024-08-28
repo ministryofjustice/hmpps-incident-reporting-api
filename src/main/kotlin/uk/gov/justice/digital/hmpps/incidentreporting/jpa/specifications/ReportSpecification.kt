185e6e2822c225027293a9d13e66ebca5a6c99e0
Igor <igor.ushkarev@digital.justice.gov.uk>
2024-08-28 12:25:02 +0100

    Allow filtering paginated reports list by staff or prisoner involvement

--- src/main/kotlin/uk/gov/justice/digital/hmpps/incidentreporting/jpa/specifications/ReportSpecification.kt
+++ src/main/kotlin/uk/gov/justice/digital/hmpps/incidentreporting/jpa/specifications/ReportSpecification.kt
@@ -1,9 +1,13 @@
 package uk.gov.justice.digital.hmpps.incidentreporting.jpa.specifications
 
+import jakarta.persistence.criteria.Join
+import org.springframework.data.jpa.domain.Specification
 import uk.gov.justice.digital.hmpps.incidentreporting.constants.InformationSource
 import uk.gov.justice.digital.hmpps.incidentreporting.constants.Status
 import uk.gov.justice.digital.hmpps.incidentreporting.constants.Type
+import uk.gov.justice.digital.hmpps.incidentreporting.jpa.PrisonerInvolvement
 import uk.gov.justice.digital.hmpps.incidentreporting.jpa.Report
+import uk.gov.justice.digital.hmpps.incidentreporting.jpa.StaffInvolvement
 import java.time.LocalDate
 
 fun filterByPrisonId(prisonId: String) = Report::prisonId.buildSpecForEqualTo(prisonId)
@@ -26,3 +30,17 @@ fun filterByReportedDateFrom(date: LocalDate) =
 
 fun filterByReportedDateUntil(date: LocalDate) =
   Report::reportedAt.buildSpecForLessThan(date.plusDays(1).atStartOfDay())
+
+fun filterByInvolvedStaff(staffUsername: String): Specification<Report> {
+  return Specification { root, _, criteriaBuilder ->
+    val staffInvolved: Join<StaffInvolvement, Report> = root.join("staffInvolved")
+    criteriaBuilder.equal(staffInvolved.get<String>("staffUsername"), staffUsername)
+  }
+}
+
+fun filterByInvolvedPrisoner(prisonerNumber: String): Specification<Report> {
+  return Specification { root, _, criteriaBuilder ->
+    val prisonersInvolved: Join<PrisonerInvolvement, Report> = root.join("prisonersInvolved")
+    criteriaBuilder.equal(prisonersInvolved.get<String>("prisonerNumber"), prisonerNumber)
+  }
+}
