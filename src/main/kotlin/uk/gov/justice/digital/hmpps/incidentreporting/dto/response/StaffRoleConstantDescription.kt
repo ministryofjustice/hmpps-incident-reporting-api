9af6a25ceb91823c95179d7e91beda5f1fee5a5c
Igor <igor.ushkarev@digital.justice.gov.uk>
2024-11-05 22:03:41 +0000

    Expose NOMIS codes for all constants

---
+++ src/main/kotlin/uk/gov/justice/digital/hmpps/incidentreporting/dto/response/StaffRoleConstantDescription.kt
@@ -0,0 +1,17 @@
+package uk.gov.justice.digital.hmpps.incidentreporting.dto.response
+
+import com.fasterxml.jackson.annotation.JsonInclude
+import io.swagger.v3.oas.annotations.media.Schema
+
+@Schema(description = "Staff role constant", accessMode = Schema.AccessMode.READ_ONLY)
+@JsonInclude(JsonInclude.Include.ALWAYS)
+data class StaffRoleConstantDescription(
+  @Schema(description = "Machine-readable identifier of this value", example = "ACTIVELY_INVOLVED")
+  val code: String,
+  @Schema(description = "Human-readable description of this value", example = "Actively involved")
+  val description: String,
+
+  // NB: this property can be removed once fully migrated off NOMIS and reconciliation checks are turned off
+  @Schema(description = "Machine-readable NOMIS identifiers of this value, which may be empty for newer staff roles", example = "[AI, INV]", deprecated = true)
+  val nomisCodes: List<String>,
+)
