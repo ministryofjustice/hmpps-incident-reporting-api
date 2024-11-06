cab198c785a784bfe694b10b3b05cffe1d5d2123
ushkarev <ushkarev@users.noreply.github.com>
2024-11-06 10:02:14 +0000

    IR-573: Update DB seed with new test users (#200)

--- src/main/resources/db/seed/R__01_docker_seed.sql
+++ src/main/resources/db/seed/R__01_docker_seed.sql
@@ -1,31 +1,64 @@
-DO
+do
 $$
     declare
         v_event_id  UUID;
         v_report_id UUID;
 
-    BEGIN
-        select gen_random_uuid() INTO v_event_id;
+    begin
+        select gen_random_uuid() into v_event_id;
 
-        insert into event (id, event_reference, event_date_and_time, title, description, prison_id, created_at,
-                           modified_by)
+        insert into event (
+            id, event_reference,
+            title, description,
+            event_date_and_time, prison_id,
+            created_at, modified_by
+        )
+        values (
+            v_event_id, nextval('event_sequence'),
+            'Test Incident', 'A test incident',
+            now(), 'MDI',
+            now(), 'IR_REPORTING_OFFICER'
+        );
 
-        values (v_event_id, nextval('event_sequence'), now(), 'Test Incident', 'A test incident', 'MDI',
-                now(), 'IR_CREATOR');
+        insert into report (
+            id, event_id, report_reference,
+            title, description,
+            type, status, source,
+            incident_date_and_time, prison_id,
+            reported_at, reported_by, assigned_to,
+            created_at,
+            modified_by
+        )
+        values (
+            gen_random_uuid(), v_event_id, nextval('report_sequence'),
+            'Assault', 'Details of assualt on staff',
+            'ASSAULT', 'DRAFT', 'NOMIS',
+            now(), 'MDI',
+            now(), 'IR_REPORTING_OFFICER', 'IR_REPORTING_OFFICER',
+            now(), 'IR_REPORTING_OFFICER'
+        )
+        returning id into v_report_id;
 
-        insert into report (id, event_id, report_reference, title, description, prison_id, type, source, status,
-                            incident_date_and_time, reported_at, reported_by, assigned_to, question_set_id, created_at,
-                            modified_by)
-        values (gen_random_uuid(), v_event_id, nextval('report_sequence'), 'Assault', 'Details of assualt on staff',
-                'MDI', 'ASSAULT',
-                'NOMIS', 'DRAFT', now(), now(), 'IR_CREATOR', 'IR_VIEWER', null, now(), 'IR_CREATOR')
-        RETURNING id INTO v_report_id;
+        insert into prisoner_involvement (
+            report_id,
+            prisoner_number, prisoner_role, outcome,
+            comment
+        )
+        values (
+            v_report_id,
+            'A1234AA', 'FIGHTER', 'PLACED_ON_REPORT',
+            'Adjudication was held'
+        );
 
-        insert into prisoner_involvement (report_id, prisoner_number, prisoner_role, outcome, comment)
-        values (v_report_id, 'A1234AA', 'FIGHTER', 'PLACED_ON_REPORT', 'Adjudication was held');
-
-        insert into staff_involvement (report_id, staff_username, staff_role, comment)
-        values (v_report_id, 'ITAG_USER', 'VICTIM', 'Staff had a black eye');
-
-    END;
+        insert into staff_involvement (
+            report_id,
+            staff_username, staff_role,
+            comment
+        )
+        values (
+            v_report_id,
+            'ITAG_USER', 'VICTIM',
+            'Staff had a black eye'
+        );
+    end;
 $$
