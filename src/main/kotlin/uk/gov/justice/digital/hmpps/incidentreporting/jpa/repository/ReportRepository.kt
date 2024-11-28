34e8394dc55aee98f664fe22e25cefe390630a41
Michael Willis <michael.willis@digital.justice.gov.uk>
2024-11-28 09:41:20 +0000

    IR-698 Prevent transaction contention

--- src/main/kotlin/uk/gov/justice/digital/hmpps/incidentreporting/jpa/repository/ReportRepository.kt
+++ src/main/kotlin/uk/gov/justice/digital/hmpps/incidentreporting/jpa/repository/ReportRepository.kt
@@ -1,15 +1,21 @@
 package uk.gov.justice.digital.hmpps.incidentreporting.jpa.repository
 
+import jakarta.persistence.LockModeType
+import jakarta.persistence.QueryHint
 import org.springframework.data.jpa.repository.EntityGraph
 import org.springframework.data.jpa.repository.JpaRepository
 import org.springframework.data.jpa.repository.JpaSpecificationExecutor
+import org.springframework.data.jpa.repository.Lock
 import org.springframework.data.jpa.repository.Query
+import org.springframework.data.jpa.repository.QueryHints
 import org.springframework.stereotype.Repository
 import uk.gov.justice.digital.hmpps.incidentreporting.jpa.Report
 import java.util.UUID
 
 @Repository
 interface ReportRepository : JpaRepository<Report, UUID>, JpaSpecificationExecutor<Report> {
+  @Lock(LockModeType.PESSIMISTIC_WRITE)
+  @QueryHints(value = [QueryHint(name = "jakarta.persistence.lock.timeout", value = "2000")])
   @EntityGraph(value = "Report.eager", type = EntityGraph.EntityGraphType.FETCH)
   fun findOneEagerlyById(id: UUID): Report?
 
