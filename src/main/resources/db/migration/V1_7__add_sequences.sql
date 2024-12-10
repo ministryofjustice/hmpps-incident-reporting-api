ab13a0dbdc203f7af1f08fd80e0617afb0f34bd1
Michael Willis <michael.willis@digital.justice.gov.uk>
2024-12-10 13:24:05 +0000

    IR-714 Update existing sequences

--- src/main/resources/db/migration/V1_7__add_sequences.sql
+++ src/main/resources/db/migration/V1_7__add_sequences.sql
@@ -1,36 +1,3 @@
 alter table correction_request add column sequence integer default 0 not null;
 alter table staff_involvement add column sequence integer default 0 not null;
 alter table prisoner_involvement add column sequence integer default 0 not null;
-
--- Update correction_request sequences by report_id
-WITH numbered_rows AS (
-  SELECT id,
-         ROW_NUMBER() OVER (PARTITION BY report_id ORDER BY correction_requested_at) - 1 as new_sequence
-  FROM correction_request
-)
-UPDATE correction_request
-SET sequence = numbered_rows.new_sequence
-FROM numbered_rows
-WHERE correction_request.id = numbered_rows.id;
-
--- Update staff_involvement sequences by report_id
-WITH numbered_rows AS (
-    SELECT id,
-           ROW_NUMBER() OVER (PARTITION BY report_id ORDER BY staff_username, staff_role, comment) - 1 as new_sequence
-    FROM staff_involvement
-)
-UPDATE staff_involvement
-SET sequence = numbered_rows.new_sequence
-FROM numbered_rows
-WHERE staff_involvement.id = numbered_rows.id;
-
--- Update prisoner_involvement sequences by report_id
-WITH numbered_rows AS (
-    SELECT id,
-           ROW_NUMBER() OVER (PARTITION BY report_id ORDER BY prisoner_number, prisoner_role, outcome) - 1 as new_sequence
-    FROM prisoner_involvement
-)
-UPDATE prisoner_involvement
-SET sequence = numbered_rows.new_sequence
-FROM numbered_rows
-WHERE prisoner_involvement.id = numbered_rows.id;
