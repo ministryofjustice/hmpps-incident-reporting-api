74aff984ab81ea150759aefb2482891d17b9eb52
Michael Willis <michael.willis@digital.justice.gov.uk>
2024-12-18 18:40:19 +0000

    IR-735 remove detailed audit log changes when sync from NOMIS

--- src/main/kotlin/uk/gov/justice/digital/hmpps/incidentreporting/resource/EventBaseResource.kt
+++ src/main/kotlin/uk/gov/justice/digital/hmpps/incidentreporting/resource/EventBaseResource.kt
@@ -31,4 +31,20 @@ abstract class EventBaseResource {
         auditData = report,
       )
     }
+
+  protected fun eventPublishAndAuditNomisEvent(
+    event: ReportDomainEventType,
+    whatChanged: WhatChanged? = null,
+    report: ReportBasic,
+  ) {
+    eventPublishAndAuditService.publishEvent(
+      eventType = event,
+      additionalInformation = AdditionalInformation(
+        id = report.id,
+        reportReference = report.reportReference,
+        source = InformationSource.NOMIS,
+        whatChanged = whatChanged,
+      ),
+    )
+  }
 }
