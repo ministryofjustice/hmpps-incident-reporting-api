b2f870caa44153820b202b4ac99a6ca9ed48079a
Michael Willis <michael.willis@digital.justice.gov.uk>
2024-12-27 15:10:29 +0000

    IR-737 Change back to pessimistic write to try to prevent deadlocks

--- src/main/kotlin/uk/gov/justice/digital/hmpps/incidentreporting/jpa/repository/ReportRepository.kt
+++ src/main/kotlin/uk/gov/justice/digital/hmpps/incidentreporting/jpa/repository/ReportRepository.kt
@@ -17,7 +17,7 @@ interface ReportRepository : JpaRepository<Report, UUID>, JpaSpecificationExecut
   @EntityGraph(value = "Report.eager", type = EntityGraph.EntityGraphType.LOAD)
   fun findOneEagerlyById(id: UUID): Report?
 
-  @Lock(LockModeType.PESSIMISTIC_READ)
+  @Lock(LockModeType.PESSIMISTIC_WRITE)
   @QueryHints(value = [QueryHint(name = "jakarta.persistence.lock.timeout", value = "1000")])
   @Query(value = "SELECT r.id FROM Report r WHERE r.id = :id")
   fun findReportByIdAndLockRecord(id: UUID): UUID?
