7c7d23c4f567e88dd3ae13af8aebc6f1eaf334a1
Michael Willis <michael.willis@digital.justice.gov.uk>
2025-02-13 10:32:47 +0000

    MAP-778 Remove policy and wire in roles to endpoints

--- src/main/kotlin/uk/gov/justice/digital/hmpps/incidentreporting/HmppsIncidentReportingApi.kt
+++ src/main/kotlin/uk/gov/justice/digital/hmpps/incidentreporting/HmppsIncidentReportingApi.kt
@@ -1,13 +1,11 @@
 package uk.gov.justice.digital.hmpps.incidentreporting
 
 import org.springframework.boot.autoconfigure.SpringBootApplication
-import org.springframework.boot.context.properties.EnableConfigurationProperties
 import org.springframework.boot.runApplication
 
 const val SYSTEM_USERNAME = "INCIDENT_REPORTING_API"
 
 @SpringBootApplication
-@EnableConfigurationProperties
 class HmppsIncidentReportingApi
 
 fun main(args: Array<String>) {
