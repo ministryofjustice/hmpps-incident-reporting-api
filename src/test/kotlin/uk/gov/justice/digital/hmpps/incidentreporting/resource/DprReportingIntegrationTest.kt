bbca2a13e8a7ef7910287e76e5a60e96301642a8
Michael Willis <michael.willis@digital.justice.gov.uk>
2025-02-15 18:53:28 +0000

    IR-778 switch to INCIDENT_REPORTS__RO user role for now, and improve some reports - the date filters break the front end at the moment (#272)
    * IR-778 switch to INCIDENT_REPORTS__RO user role for now, and improve some reports - the date filters break the front end at the moment
    
    * Enhance incident reports by adding type and status descriptions, and improving name formatting for prisoner and staff involvement

--- src/test/kotlin/uk/gov/justice/digital/hmpps/incidentreporting/resource/DprReportingIntegrationTest.kt
+++ src/test/kotlin/uk/gov/justice/digital/hmpps/incidentreporting/resource/DprReportingIntegrationTest.kt
@@ -63,7 +63,7 @@ class DprReportingIntegrationTest : SqsIntegrationTestBase() {
       @TestFactory
       fun endpointRequiresAuthorisation() = endpointRequiresAuthorisation(
         webTestClient.get().uri(url),
-        "VIEW_INCIDENT_REPORTS",
+        "INCIDENT_REPORTS__RO",
       )
     }
 
@@ -74,7 +74,7 @@ class DprReportingIntegrationTest : SqsIntegrationTestBase() {
       @Test
       fun `returns the definitions of all the reports`() {
         webTestClient.get().uri(url)
-          .headers(setAuthorisation(roles = listOf("ROLE_VIEW_INCIDENT_REPORTS"), scopes = listOf("read")))
+          .headers(setAuthorisation(roles = listOf("ROLE_INCIDENT_REPORTS__RO"), scopes = listOf("read")))
           .header("Content-Type", "application/json")
           .exchange()
           .expectStatus().isOk
@@ -95,7 +95,7 @@ class DprReportingIntegrationTest : SqsIntegrationTestBase() {
       @TestFactory
       fun endpointRequiresAuthorisation() = endpointRequiresAuthorisation(
         webTestClient.get().uri(url),
-        "VIEW_INCIDENT_REPORTS",
+        "INCIDENT_REPORTS__RO",
       )
     }
 
@@ -106,7 +106,7 @@ class DprReportingIntegrationTest : SqsIntegrationTestBase() {
       @Test
       fun `returns the definition of the report`() {
         webTestClient.get().uri(url)
-          .headers(setAuthorisation(roles = listOf("ROLE_VIEW_INCIDENT_REPORTS"), scopes = listOf("read")))
+          .headers(setAuthorisation(roles = listOf("ROLE_INCIDENT_REPORTS__RO"), scopes = listOf("read")))
           .header("Content-Type", "application/json")
           .exchange()
           .expectStatus().isOk
@@ -122,268 +122,6 @@ class DprReportingIntegrationTest : SqsIntegrationTestBase() {
     "name": "Incident Report Summary",
     "resourceName": "reports/incident-report/summary",
     "description": "List of all incidents filtered by dates, types, status and locations",
-    "specification": {
-      "template": "list",
-      "fields": [
-        {
-          "name": "id",
-          "display": "Internal ID",
-          "sortable": true,
-          "defaultsort": false,
-          "type": "string"
-        },
-        {
-          "name": "report_reference",
-          "display": "Report Reference"
-        },
-        {
-          "name": "type",
-          "display": "Incident type",
-          "filter": {
-            "type": "autocomplete",
-            "mandatory": false,
-            "pattern": null,
-            "staticOptions": [
-              {
-                "name": "ABSCONDER",
-                "display": "Absconder"
-              },
-              {
-                "name": "ASSAULT",
-                "display": "Assault"
-              },
-              {
-                "name": "ATTEMPTED_ESCAPE_FROM_CUSTODY",
-                "display": "Attempted escape from custody"
-              },
-              {
-                "name": "ATTEMPTED_ESCAPE_FROM_ESCORT",
-                "display": "Attempted escape from escort"
-              },
-              {
-                "name": "BOMB_THREAT",
-                "display": "Bomb threat"
-              },
-              {
-                "name": "BREACH_OF_SECURITY",
-                "display": "Breach of security"
-              },
-              {
-                "name": "DAMAGE",
-                "display": "Damage"
-              },
-              {
-                "name": "DEATH_IN_CUSTODY",
-                "display": "Death in custody"
-              },
-              {
-                "name": "DEATH_OTHER",
-                "display": "Death (other)"
-              },
-              {
-                "name": "DISORDER",
-                "display": "Disorder"
-              },
-              {
-                "name": "DRONE_SIGHTING",
-                "display": "Drone sighting"
-              },
-              {
-                "name": "ESCAPE_FROM_CUSTODY",
-                "display": "Escape from custody"
-              },
-              {
-                "name": "ESCAPE_FROM_ESCORT",
-                "display": "Escape from escort"
-              },
-              {
-                "name": "FINDS",
-                "display": "Finds"
-              },
-              {
-                "name": "FIRE",
-                "display": "Fire"
-              },
-              {
-                "name": "FOOD_REFUSAL",
-                "display": "Food refusal"
-              },
-              {
-                "name": "FULL_CLOSE_DOWN_SEARCH",
-                "display": "Full close down search"
-              },
-              {
-                "name": "KEY_LOCK_INCIDENT",
-                "display": "Key lock incident"
-              },
-              {
-                "name": "MISCELLANEOUS",
-                "display": "Miscellaneous"
-              },
-              {
-                "name": "OLD_ASSAULT",
-                "display": "Assault"
-              }
-            ]
-          },
-          "sortable": true,
-          "defaultsort": false,
-          "type": "string",
-          "mandatory": false,
-          "visible": true,
-          "calculated": false,
-          "header": false
-        },
-        {
-          "name": "status",
-          "display": "Incident status",
-          "wordWrap": null,
-          "filter": {
-            "type": "autocomplete",
-            "mandatory": false,
-            "pattern": null,
-            "staticOptions": [
-              {
-                "name": "AWAITING_ANALYSIS",
-                "display": "Awaiting analysis"
-              },
-              {
-                "name": "CLOSED",
-                "display": "Closed"
-              },
-              {
-                "name": "DRAFT",
-                "display": "Draft"
-              },
-              {
-                "name": "DUPLICATE",
-                "display": "Duplicate"
-              },
-              {
-                "name": "IN_ANALYSIS",
-                "display": "In analysis"
-              },
-              {
-                "name": "INCIDENT_UPDATED",
-                "display": "Incident updated"
-              },
-              {
-                "name": "INFORMATION_AMENDED",
-                "display": "Information amened"
-              },
-              {
-                "name": "INFORMATION_REQUIRED",
-                "display": "Information required"
-              },
-              {
-                "name": "POST_INCIDENT_UPDATE",
-                "display": "Post-incident update"
-              }
-            ]
-          },
-          "sortable": true,
-          "defaultsort": false,
-          "type": "string",
-          "mandatory": false,
-          "visible": true,
-          "calculated": false,
-          "header": false
-        },
-        {
-          "name": "incident_date_and_time",
-          "display": "Incident occurrence time",
-          "wordWrap": null,
-          "filter": {
-            "type": "daterange",
-            "mandatory": false
-          },
-          "sortable": false,
-          "defaultsort": false,
-          "type": "date",
-          "mandatory": false,
-          "visible": true,
-          "calculated": false,
-          "header": false
-        },
-        {
-          "name": "reported_at",
-          "display": "Reported on",
-          "wordWrap": null,
-          "filter": {
-            "type": "daterange",
-            "mandatory": false
-          },
-          "sortable": true,
-          "defaultsort": false,
-          "type": "date",
-          "mandatory": false,
-          "visible": true,
-          "calculated": false,
-          "header": false
-        },
-        {
-          "name": "reported_by",
-          "display": "Reported by",
-          "sortable": false,
-          "defaultsort": false,
-          "type": "string",
-          "mandatory": false,
-          "visible": true,
-          "calculated": false,
-          "header": false
-        },
-        {
-          "name": "title",
-          "display": "Incident title",
-          "sortable": false,
-          "defaultsort": false,
-          "type": "string",
-          "mandatory": false,
-          "visible": true,
-          "calculated": false,
-          "header": false
-        },
-        {
-          "name": "description",
-          "display": "Incident description",
-          "sortable": false,
-          "defaultsort": false,
-          "type": "string",
-          "mandatory": false,
-          "visible": true,
-          "calculated": false,
-          "header": false
-        },
-        {
-          "name": "location",
-          "display": "Location of incident",
-          "sortable": true,
-          "defaultsort": false,
-          "type": "string",
-          "mandatory": false,
-          "visible": true,
-          "calculated": false,
-          "header": false
-        },
-        {
-          "name": "modified_at",
-          "display": "Last Updated",
-          "wordWrap": null,
-          "filter": {
-            "type": "daterange",
-            "mandatory": false,
-            "interactive": false
-          },
-          "sortable": true,
-          "defaultsort": true,
-          "type": "date",
-          "mandatory": false,
-          "visible": true,
-          "calculated": false,
-          "header": false
-        }
-      ]
-    },
     "printable": true
   }
 }
@@ -405,7 +143,7 @@ class DprReportingIntegrationTest : SqsIntegrationTestBase() {
       @TestFactory
       fun endpointRequiresAuthorisation() = endpointRequiresAuthorisation(
         webTestClient.get().uri(url),
-        "VIEW_INCIDENT_REPORTS",
+        "INCIDENT_REPORTS__RO",
       )
     }
 
@@ -416,7 +154,7 @@ class DprReportingIntegrationTest : SqsIntegrationTestBase() {
       @Test
       fun `returns a page of the report`() {
         webTestClient.get().uri(url)
-          .headers(setAuthorisation(roles = listOf("ROLE_VIEW_INCIDENT_REPORTS"), scopes = listOf("read")))
+          .headers(setAuthorisation(roles = listOf("ROLE_INCIDENT_REPORTS__RO"), scopes = listOf("read")))
           .header("Content-Type", "application/json")
           .exchange()
           .expectStatus().isOk
@@ -455,7 +193,7 @@ class DprReportingIntegrationTest : SqsIntegrationTestBase() {
       @TestFactory
       fun endpointRequiresAuthorisation() = endpointRequiresAuthorisation(
         webTestClient.get().uri(url + "/by-prisoner"),
-        "VIEW_INCIDENT_REPORTS",
+        "INCIDENT_REPORTS__RO",
       )
     }
 
@@ -466,7 +204,7 @@ class DprReportingIntegrationTest : SqsIntegrationTestBase() {
       @Test
       fun `returns a page of the report for staff`() {
         webTestClient.get().uri(url + "/by-staff")
-          .headers(setAuthorisation(roles = listOf("ROLE_VIEW_INCIDENT_REPORTS"), scopes = listOf("read")))
+          .headers(setAuthorisation(roles = listOf("ROLE_INCIDENT_REPORTS__RO"), scopes = listOf("read")))
           .header("Content-Type", "application/json")
           .exchange()
           .expectStatus().isOk
@@ -518,7 +256,7 @@ class DprReportingIntegrationTest : SqsIntegrationTestBase() {
       @Test
       fun `returns a page of the report for prisoners`() {
         webTestClient.get().uri(url + "/by-prisoner")
-          .headers(setAuthorisation(roles = listOf("ROLE_VIEW_INCIDENT_REPORTS"), scopes = listOf("read")))
+          .headers(setAuthorisation(roles = listOf("ROLE_INCIDENT_REPORTS__RO"), scopes = listOf("read")))
           .header("Content-Type", "application/json")
           .exchange()
           .expectStatus().isOk
@@ -535,7 +273,6 @@ class DprReportingIntegrationTest : SqsIntegrationTestBase() {
     "reported_by": "USER1",
     "first_name": "First 1",
     "last_name": "Last 1",
-    "prisoner_number": "A0001AA",
     "comment": "Comment #1"
   },
   {
@@ -547,7 +284,6 @@ class DprReportingIntegrationTest : SqsIntegrationTestBase() {
     "reported_by": "USER1",
     "first_name": "First 2",
     "last_name": "Last 2",
-    "prisoner_number": "A0002AA",
     "comment": "Comment #2"
   }
 ]
@@ -569,7 +305,7 @@ class DprReportingIntegrationTest : SqsIntegrationTestBase() {
       @TestFactory
       fun endpointRequiresAuthorisation() = endpointRequiresAuthorisation(
         webTestClient.get().uri(url + "by-week"),
-        "VIEW_INCIDENT_REPORTS",
+        "INCIDENT_REPORTS__RO",
       )
     }
 
@@ -580,7 +316,7 @@ class DprReportingIntegrationTest : SqsIntegrationTestBase() {
       @Test
       fun `returns a page of the report for a count by day`() {
         webTestClient.get().uri(url + "by-day")
-          .headers(setAuthorisation(roles = listOf("ROLE_VIEW_INCIDENT_REPORTS"), scopes = listOf("read")))
+          .headers(setAuthorisation(roles = listOf("ROLE_INCIDENT_REPORTS__RO"), scopes = listOf("read")))
           .header("Content-Type", "application/json")
           .exchange()
           .expectStatus().isOk
@@ -589,7 +325,7 @@ class DprReportingIntegrationTest : SqsIntegrationTestBase() {
             """
 [
   {
-    "start_date": "2023-12-05T00:00:00",
+    "start_date": "2023-12-05",
     "location": "MDI",
     "type": "FINDS",
     "status": "DRAFT",
@@ -603,7 +339,7 @@ class DprReportingIntegrationTest : SqsIntegrationTestBase() {
       @Test
       fun `returns a page of the report for a count by week`() {
         webTestClient.get().uri(url + "by-week")
-          .headers(setAuthorisation(roles = listOf("ROLE_VIEW_INCIDENT_REPORTS"), scopes = listOf("read")))
+          .headers(setAuthorisation(roles = listOf("ROLE_INCIDENT_REPORTS__RO"), scopes = listOf("read")))
           .header("Content-Type", "application/json")
           .exchange()
           .expectStatus().isOk
@@ -612,7 +348,7 @@ class DprReportingIntegrationTest : SqsIntegrationTestBase() {
             """
 [
   {
-    "start_date": "2023-12-04T00:00:00",
+    "start_date": "2023-12-04",
     "location": "MDI",
     "type": "FINDS",
     "status": "DRAFT",
@@ -626,7 +362,7 @@ class DprReportingIntegrationTest : SqsIntegrationTestBase() {
       @Test
       fun `returns a page of the report for a count by month`() {
         webTestClient.get().uri(url + "by-month")
-          .headers(setAuthorisation(roles = listOf("ROLE_VIEW_INCIDENT_REPORTS"), scopes = listOf("read")))
+          .headers(setAuthorisation(roles = listOf("ROLE_INCIDENT_REPORTS__RO"), scopes = listOf("read")))
           .header("Content-Type", "application/json")
           .exchange()
           .expectStatus().isOk
@@ -635,7 +371,7 @@ class DprReportingIntegrationTest : SqsIntegrationTestBase() {
             """
 [
   {
-    "start_date": "2023-12-01T00:00:00",
+    "start_date": "2023-12-01",
     "location": "MDI",
     "type": "FINDS",
     "status": "DRAFT",
@@ -649,7 +385,7 @@ class DprReportingIntegrationTest : SqsIntegrationTestBase() {
       @Test
       fun `returns a page of the report for a count by location per week`() {
         webTestClient.get().uri(url + "by-location-per-week")
-          .headers(setAuthorisation(roles = listOf("ROLE_VIEW_INCIDENT_REPORTS"), scopes = listOf("read")))
+          .headers(setAuthorisation(roles = listOf("ROLE_INCIDENT_REPORTS__RO"), scopes = listOf("read")))
           .header("Content-Type", "application/json")
           .exchange()
           .expectStatus().isOk
@@ -658,7 +394,7 @@ class DprReportingIntegrationTest : SqsIntegrationTestBase() {
             """
 [
   {
-    "start_date": "2023-12-04T00:00:00",
+    "start_date": "2023-12-04",
     "location": "MDI",
     "type": "FINDS",
     "num_of_incidents": 1
