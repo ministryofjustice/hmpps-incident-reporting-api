3da9f762c8a15dfce7b5bba4974a448414cc58fe
Michael Willis <michael.willis@digital.justice.gov.uk>
2025-05-06 12:37:04 +0100

    IR-1035 Refactor views into dedicated "report" schema.
    Moved materialized views and indexes into the "report" schema for improved organization and clarity. Updated SQL queries and service logic to reference the new schema structure. This change ensures better separation of concerns and aligns with database design best practices.

--- src/main/resources/db/views/R__102_self_harm_indexes.sql
+++
@@ -1,3 +0,0 @@
-
-create index if not exists self_harm_summary_view_idx
-  on self_harm_summary_view (report_id, incident_date_and_time, location);
--- src/main/resources/db/views/R__102_self_harm_view.sql
+++ src/main/resources/db/views/R__102_self_harm_view.sql
@@ -1,7 +1,6 @@
-drop materialized view if exists self_harm_summary;
-drop materialized view if exists self_harm_summary_view;
+drop materialized view if exists report.self_harm_summary_view;
 
-CREATE MATERIALIZED VIEW self_harm_summary_view
+CREATE MATERIALIZED VIEW report.self_harm_summary_view
 AS
 select r.id      as report_id,
        r.report_reference,
@@ -15,30 +14,30 @@ select r.id      as report_id,
        pi.first_name,
        (select q1res.response
         from question q
-                 join response q1res on q1res.question_id = q.id
+               join response q1res on q1res.question_id = q.id
         where q.report_id = r.id
           and q.question = 'WHERE DID THE INCIDENT TAKE PLACE'
           and q1res.sequence = 0
         limit 1) as q1_location,
        (select q1res.additional_information
         from question q
-                 join response q1res on q1res.question_id = q.id
+               join response q1res on q1res.question_id = q.id
         where q.report_id = r.id
           and q.question = 'WHERE DID THE INCIDENT TAKE PLACE'
           and q1res.sequence = 0
         limit 1) as additional_comment,
        (select string_agg(
-                       case
-                           when q.question = 'DID SELF HARM METHOD INVOLVE CUTTING' then 'Cutting'
-                           when q.question = 'DID SELF HARM METHOD INVOLVE HANGING' then 'Hanging'
-                           when q.question = 'DID SELF HARM METHOD INVOLVE SELF STRANGULATION' then 'Self Strangulation'
-                           when q.question = 'DID SELF HARM INVOLVE SELF POISONING/ OVERDOSE/SWALLOWING OBJECTS'
-                               then 'Poisoning / Overdose / Swallowing'
-                           when q.question = 'DID SELF HARM METHOD INVOLVE BURNING' then 'Burning'
-                           else 'Other' end,
-                       ', ')
+                 case
+                   when q.question = 'DID SELF HARM METHOD INVOLVE CUTTING' then 'Cutting'
+                   when q.question = 'DID SELF HARM METHOD INVOLVE HANGING' then 'Hanging'
+                   when q.question = 'DID SELF HARM METHOD INVOLVE SELF STRANGULATION' then 'Self Strangulation'
+                   when q.question = 'DID SELF HARM INVOLVE SELF POISONING/ OVERDOSE/SWALLOWING OBJECTS'
+                     then 'Poisoning / Overdose / Swallowing'
+                   when q.question = 'DID SELF HARM METHOD INVOLVE BURNING' then 'Burning'
+                   else 'Other' end,
+                 ', ')
         from question q
-                 join response res on res.question_id = q.id
+               join response res on res.question_id = q.id
         where q.report_id = r.id
           and q.question in ('DID SELF HARM METHOD INVOLVE CUTTING',
                              'DID SELF HARM METHOD INVOLVE HANGING',
@@ -50,7 +49,7 @@ select r.id      as report_id,
 
        (select string_agg(res.response, ', ')
         from question q
-                 join response res on res.question_id = q.id
+               join response res on res.question_id = q.id
         where q.report_id = r.id
           and q.question in ('TYPE OF IMPLEMENT USED',
                              'LIGATURE TYPE',
@@ -59,11 +58,14 @@ select r.id      as report_id,
                  as materials_used,
        (select string_agg(res.response, ', ')
         from question q
-                 join response res on res.question_id = q.id
+               join response res on res.question_id = q.id
         where q.report_id = r.id
           and q.question = 'WHAT OTHER METHOD OF SELF HARM WAS INVOLVED')
                  as other_method
 from report r
-         join prisoner_involvement pi on r.id = pi.report_id and pi.prisoner_role = 'PERPETRATOR'
+       join prisoner_involvement pi on r.id = pi.report_id and pi.prisoner_role = 'PERPETRATOR'
 where r.type = 'SELF_HARM_1'
   and r.status != 'DUPLICATE';
+
+create index if not exists self_harm_summary_view_idx
+  on report.self_harm_summary_view (report_id, incident_date_and_time, location);
