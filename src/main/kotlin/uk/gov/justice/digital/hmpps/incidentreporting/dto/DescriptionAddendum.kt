8d23f352b0f88c90652be0e112d7ab0b6e697614
Igor <igor.ushkarev@digital.justice.gov.uk>
2025-05-07 21:53:15 +0100

    Mark sequence numbers in all response DTOs as deprecated – they are for debugging only

--- src/main/kotlin/uk/gov/justice/digital/hmpps/incidentreporting/dto/DescriptionAddendum.kt
+++ src/main/kotlin/uk/gov/justice/digital/hmpps/incidentreporting/dto/DescriptionAddendum.kt
@@ -4,10 +4,11 @@ import com.fasterxml.jackson.annotation.JsonInclude
 import io.swagger.v3.oas.annotations.media.Schema
 import java.time.LocalDateTime
 
-@Schema(description = "Addendum to the description", accessMode = Schema.AccessMode.READ_ONLY)
+@Schema(description = "Addendum to the description of an incident", accessMode = Schema.AccessMode.READ_ONLY)
 @JsonInclude(JsonInclude.Include.ALWAYS)
 data class DescriptionAddendum(
-  @Schema(description = "Sequence of the addendums for this report", example = "0")
+  // TODO: sequences are only being exposed while we sort out sync problems: they do not need to remain in the api contract
+  @Schema(description = "Sequence of the addendums for this report", deprecated = true)
   val sequence: Int,
   @Schema(description = "Username of user who added this addendum", example = "USER_1")
   val createdBy: String,
