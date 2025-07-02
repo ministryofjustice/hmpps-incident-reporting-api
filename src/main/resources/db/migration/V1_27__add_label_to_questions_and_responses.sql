801ce015102426e3241b3758fa04d1513c5a851f
Michael Willis <Michael.Willis2@justice.gov.uk>
2025-07-02 09:56:24 +0100

    IR-1100 Fix typo in `historical_response` label update query. (#412)
    * IR-1100 Correct `historical_resposne` typo in migration script to `historical_response`.

--- src/main/resources/db/migration/V1_27__add_label_to_questions_and_responses.sql
+++ src/main/resources/db/migration/V1_27__add_label_to_questions_and_responses.sql
@@ -6,7 +6,7 @@ ALTER TABLE historical_response ADD COLUMN label TEXT;
 update question set label = question;
 update historical_question set label = question;
 update response set label = response;
-update historical_resposne set label = response;
+update historical_response set label = response;
 
 ALTER TABLE question ALTER COLUMN label SET NOT NULL;
 ALTER TABLE historical_question ALTER COLUMN label SET NOT NULL;
