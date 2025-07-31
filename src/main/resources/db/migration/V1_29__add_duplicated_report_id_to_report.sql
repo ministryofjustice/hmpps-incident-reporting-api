19c397bcfa5a3908876033f0435b415e5b6e8cc3
Josh Jones <josh.jones4@digital.justice.gov.uk>
2025-07-31 12:21:29 +0100

    Added constraint to DB migration

--- src/main/resources/db/migration/V1_29__add_duplicated_report_id_to_report.sql
+++ src/main/resources/db/migration/V1_29__add_duplicated_report_id_to_report.sql
@@ -1,1 +1,2 @@
-ALTER TABLE report ADD COLUMN duplicated_report_id uuid;
+ALTER TABLE report ADD COLUMN duplicated_report_id uuid
+  CONSTRAINT original_report_fk REFERENCES report (id) ON DELETE SET NULL;
