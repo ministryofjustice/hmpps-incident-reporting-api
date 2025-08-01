5c4450cd8a2e78a25a58a2e87429921ead769361
Michael Willis <Michael.Willis2@justice.gov.uk>
2025-08-01 13:01:22 +0100

    Remove redundant unique index creation from V1_30 migration script

--- src/main/resources/db/migration/V1_30__analytical_marker.sql
+++ src/main/resources/db/migration/V1_30__analytical_marker.sql
@@ -4,5 +4,3 @@ CREATE TABLE analytical_marker
   marker_type   varchar(60) not null,
   constraint analytical_marker_type_pk primary key (response_code, marker_type)
 );
-
-create unique index analytical_marker_pk on analytical_marker (response_code, marker_type);
